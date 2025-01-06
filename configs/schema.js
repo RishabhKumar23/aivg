7d8ecf5ebeec6aa61c4643b59afafebdeb032a57
Rishabh Kumar <112078029+RishabhKumar23@users.noreply.github.com>
2025-01-06 15:22:55 +0530

    DB conflict solved
    duplication on records in db is now solved.

--- configs/schema.js
+++ configs/schema.js
@@ -1,6 +1,6 @@
 import { boolean, pgTable, serial, varchar } from "drizzle-orm/pg-core";
 
-export const User = pgTable('users', {
+export const Users = pgTable('users', {
     id: serial('id').primaryKey(),
     name: varchar('name').notNull(),
     email: varchar('email').notNull(),
